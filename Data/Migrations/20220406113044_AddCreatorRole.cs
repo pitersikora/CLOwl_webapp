a2c8b28d54644a6fc80cec3f6bf6aa8e093c3ce5
pitersikora <piootrsikora@gmail.com>
2022-04-06 13:38:16 +0200

    Migration for creator role added

---
+++ Data/Migrations/20220406113044_AddCreatorRole.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClowlWebApp.Data.Migrations
+{
+    public partial class AddCreatorRole : Migration
+    {
+        const string CREATOR_ROLE_GUID = "31b75525-05d1-454d-ac64-0b690a5c0ce9";
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{CREATOR_ROLE_GUID}','Creator','CREATOR')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = '{CREATOR_ROLE_GUID}'");
+        }
+    }
+}
